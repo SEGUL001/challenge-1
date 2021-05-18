20ab82103a084f3957222881f5f3c46dfcaf5f44
lucas <lucassegurola@gmail.com>
2021-05-18 15:29:50 -0300

    adding firebase to prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyA5AcJhFZM1uNGFAofZPEFfmKcbumPcYMA",
+    authDomain: "challenge-intercorp-32ade.firebaseapp.com",
+    projectId: "challenge-intercorp-32ade",
+    storageBucket: "challenge-intercorp-32ade.appspot.com",
+    messagingSenderId: "280696235455",
+    appId: "1:280696235455:web:531221addbdb9d037c6d0a"
+  }
 };
